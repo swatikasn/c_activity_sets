8453fdfdbde1fd473ff8c2089c67372725dfdc7d
swatikasn <swatikasn.bsc23@rvu.edu.in>
2024-02-06 14:53:01 +0530

    test

--- set02/problem01.c
+++ set02/problem01.c
@@ -4,6 +4,7 @@ void input(float *base, float *height);
 void find_area(float base, float height, float *area);
 void output(float base, float height, float area);
 
+
 int main() {
     float base, height, area;
 
